37382d953b0ed81a134da8479de738cb94b6610c
maihubro <163737352+maihubro@users.noreply.github.com>
2024-03-31 19:34:33 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -18,6 +18,7 @@ class Config(object):
     APP_ID = int(os.environ.get("APP_ID", 6))
     ALIVE_PIC =  os.environ.get("ALIVE_PIC") or None
     API_HASH = os.environ.get("API_HASH") or None
+    PM_TEXT =  os.environ.get("PM_TEXT") or None
     # Datbase url heroku sets it automatically else get this from elephantsql
     DB_URI = os.environ.get("DATABASE_URL", None)
     # Get this value by running python3 stringsetup.py or https://repl.it/@sandeep1709/generatestringsession
